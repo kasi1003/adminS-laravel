7b116c29d427f5337e89e1c04ca9d0b71dbecf40
Imasiku Tobby <imajnr@gmail.com>
2023-11-05 20:47:36 +0200

    displays sections based on cemetery selected

--- app/Http/Livewire/BurialRecords.php
+++ app/Http/Livewire/BurialRecords.php
@@ -2,12 +2,126 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\BurialRecordsMod;
+use App\Models\Cemeteries;
+use App\Models\Regions;
+use App\Models\Sections;
+use App\Models\Towns;
 use Livewire\Component;
 
 class BurialRecords extends Component
 {
+    public $selected_grave; // Add this property to store the selected grave number
+
+    public $cemeteries;
+    public $section_select;
+    public $grave_number_select;
+    public $name;
+    public $surname;
+    public $date_of_death;
+    public $date_of_birth;
+    public $death_number;
+    public $section_code;
+
+    public $buried_records;
+    public $cemeteries_selected;
+    public $section_selected; // Add this property to store the selected section
+
+    public function mount()
+    {
+        $this->load_data();
+    }
+
+    public function load_data()
+    {
+        $this->cemeteries = Cemeteries::all();
+    }
+
+    public function updatedCemeteriesSelected()
+    {
+        $this->section_selected = null; // Clear the previous selection when a new cemetery is chosen
+    }
+
+    public function getSectionOptions()
+    {
+        $sections = [];
+
+        if ($this->cemeteries_selected != 'other') {
+            // Find the selected cemetery
+            $cemetery = Cemeteries::where('CemeteryName', $this->cemeteries_selected)->first();
+
+            if ($cemetery) {
+                // Retrieve sections with the same CemeteryID
+                $sections = Sections::where('CemeteryID', $cemetery->CemeteryID)->get();
+            }
+        }
+
+        return $sections;
+    }
+
+
+
+    public function getAvailableGraves()
+    {
+        $sectionCodes = [];
+
+        if ($this->cemeteries_selected != 'other') {
+            // Find the selected cemetery
+            $cemetery = Cemeteries::where('CemeteryName', $this->cemeteries_selected)->first();
+
+            if ($cemetery) {
+                // Retrieve sections with the same CemeteryID
+                $sections = Sections::where('CemeteryID', $cemetery->CemeteryID)->get();
+
+                // Extract SectionCode values
+                $sectionCodes = $sections->pluck('SectionCode');
+            }
+        }
+
+        return $sectionCodes;
+    }
+
+
     public function render()
     {
-        return view('livewire.burial-records');
+        $this->sections = Sections::all();
+
+
+        $buried_records = BurialRecordsMod::all();
+
+        return view('livewire.burial-records', [
+            'buried_records' => $buried_records,
+            'sectionOptions' => $this->getSectionOptions(),
+            'availableGraves' => $this->getAvailableGraves(),
+        ]);
+    }
+    public function addRecord()
+    {
+        // Validate the selected_grave here if needed
+        if ($this->selected_grave) {
+            // Find the section based on the CemeteryID
+            $cemetery = Cemeteries::where('CemeteryName', $this->cemeteries_selected)->first();
+
+            if ($cemetery) {
+                $section = Sections::where('CemeteryID', $cemetery->CemeteryID)->first();
+
+                if ($section) {
+                    // Check if the selected grave is available
+                    if ($section->AvailableGraves > 0) {
+                        // Update the BurialRecordsMod model with the selected grave
+                        $burialRecord = new BurialRecordsMod();
+                        $burialRecord->GraveNumber = $this->selected_grave;
+                        $burialRecord->save();
+
+                        // Decrease the AvailableGraves by 1
+                        $section->decrement('AvailableGraves');
+                    }
+                }
+            }
+
+            // Reset the selected_grave to null
+            $this->selected_grave = null;
+        }
     }
 }
+
--- resources/views/livewire/burial-records.blade.php
+++ resources/views/livewire/burial-records.blade.php
@@ -5,40 +5,102 @@
             <form wire:submit.prevent="addRecord">
                 <div class="mb-3">
                     <div>
-                        <label for="cemeteries_selected" class="form-label"> Cemetery</label>
+                        <label for="cemeteries_selected" class="form-label">Cemetery</label>
                     </div>
                     <div class="mb-3">
                         <select id="cemeteries_selected" name="cemeteries_selected" class="form-select p-2"
                             style="width:100%;" wire:model="cemeteries_selected">
-
                             <option selected>Select Cemetery</option>
-                            @foreach ($cemeteries as $cemetry)
-                                <option value="{{ $cemetry->CemeteryID }}">{{ $cemetry->CemeteryName }}</option>
+                            @foreach ($cemeteries as $cemetery)
+                                <option value="{{ $cemetery->CemeteryName }}">{{ $cemetery->CemeteryName }}</option>
                             @endforeach
                             <option value="other">Other</option>
                         </select>
                     </div>
+                </div>
+                
+
+                <div class="mb-3">
+                    <div>
+                        <label for="section" class="form-label">Select Section</label>
+                    </div>
+                    <div class="mb-3">
+                        <select id="section_select" name="section_select" class="form-select p-2"
+                            aria-label="Default select example" style="width:100%;" wire:model="town_selected">
+                            <option selected>Select Section</option>
+                            @foreach ($sectionOptions as $section)
+                                <option value="{{ $section->SectionCode }}_{{ $section->CemeteryID }}">{{ $section->SectionCode }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                
+                <div class="mb-3">
+                    <div>
+                        <label for="graveNumberSelect" class="form-label">Select Grave Number</label>
+                    </div>
+                    <div class="mb-3">
+                        <select id="grave_number_select" name="grave_number_select" class="form-select p-2"
+                            aria-label="Default select example" style="width:100%;" wire:model="selected_grave">
+                            <option selected>Select Grave Number</option>
+                            @foreach ($availableGraves as $grave)
+                                <option value="{{ $grave }}">{{ $grave }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                
+                
+                
+                <div class="mb-3">
+                    <div>
+                        <label for="nameInput" class="form-label">Enter Name</label>
+                    </div>
+                    <div class="input-group mb-3">
+
+                        <input type="text" id="name" name="name" class="form-control" placeholder="Name"
+                            aria-label="Name" aria-describedby="basic-addon1">
+                    </div>
+
 
                 </div>
                 <div class="mb-3">
                     <div>
-                        <label for="sectionSelect" class="form-label">Select Section</label>
+                        <label for="surnameInput" class="form-label">Enter Surname</label>
                     </div>
-                    <div class="mb-3">
-                        <select id="sectionSelect" name="sectionBuried" class="form-select p-2"
-                            aria-label="Default select example" style="width:100%;" wire:model="town_selected">
+                    <div class="input-group mb-3">
 
-                            <option selected>Select Section</option>
-                            @foreach ($buried_records as $buried_record)
-                                <option value="{{ $buried_record->id }}">{{ $buried_record->SectionCode }}</option>
-                            @endforeach
-                        </select>
+                        <input type="text" id="surname" name="surname" class="form-control" placeholder="Surname"
+                            aria-label="Surname" aria-describedby="basic-addon2">
                     </div>
+
 
                 </div>
+                <div class="mb-3">
+                    <div>
+                        <label for="dateOfBirthInput" class="form-label">Date of Birth</label>
+                    </div>
+                    <div class="input-group mb-3">
 
+                        <input type="date" id="dateOfBirth" name="dateOfBirth" class="form-control"
+                            placeholder="Enter Persons Birthday" aria-label="Date of Birth"
+                            aria-describedby="basic-addon3">
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <div>
+                        <label for="dateOfDeathInput" class="form-label">Date of Birth</label>
+                    </div>
+                    <div class="input-group mb-3">
+
+                        <input type="date" id="dateOfDeath" name="dateOfDeath" class="form-control"
+                            placeholder="Enter Date of Death" aria-label="Date of Birth"
+                            aria-describedby="basic-addon3">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-          
+
         </div>
-      </div>
+    </div>
 </div>
