6c67d7731b85b27008f37e78ef1e974b6dca37cf
Imasiku Tobby <imajnr@gmail.com>
2024-04-03 16:28:02 +0200

    Failing to display Row based on selected Section and Cemetery

--- app/Http/Livewire/BurialRecords.php
+++ app/Http/Livewire/BurialRecords.php
@@ -64,34 +64,13 @@ class BurialRecords extends Component
 
         return $sections;
     }
-    public function updatedSectionSelect($value)
-    {
-        // Extract CemeteryID and SectionCode from the selected value
-        list($sectionCode, $cemeteryId) = explode('_', $value);
-        // Fetch the CemeteryID using the Cemetery name
-        $cemetery = Cemeteries::where('CemeteryName', $this->cemeteries_selected)->first();
 
-        // Fetch RowID values based on CemeteryID and SectionCode
-        $this->rowOptions = Graves::select('RowID')
-            ->where('CemeteryID', $cemetery)
-            ->where('SectionCode', $sectionCode)
-            ->distinct()
-            ->get();
-    }
-
+  
 
     public function render()
     {
-        $this->sections = Sections::all();
-
-
-        $rows = Graves::pluck('RowID')->unique()->toArray();
-
-
         return view('livewire.burial-records', [
-            'buried_records' => Graves::all(),
             'sectionOptions' => $this->getSectionOptions(),
-
         ]);
     }
 
@@ -99,46 +78,6 @@ class BurialRecords extends Component
 
     public function addRecord()
     {
-        // Find the selected cemetery
-        $cemetery = Cemeteries::where('CemeteryName', $this->cemeteries_selected)->first();
-
-        if ($cemetery) {
-            // Decrease the AvailableGraves by 1
-            $cemetery->decrement('AvailableGraves');
-
-            // Create the DeathNumber by combining CemeteryName, SectionCode, and selected_grave
-            $deathNumber = $this->cemeteries_selected . $this->section_select . $this->selected_grave;
-
-            // Create a data array for the new record
-            $data = [
-                'CemeteryID' => $cemetery->CemeteryID,
-                'SectionCode' => $this->section_select,
-                'GraveNumber' => $this->selected_grave,
-                'Name' => $this->name,
-                'Surname' => $this->surname,
-                'DateOfBirth' => $this->date_of_birth,
-                'DateOfDeath' => $this->date_of_death,
-                'DeathNumber' => $deathNumber,
-            ];
-
-            // Create a new Grave record
-            Graves::create($data);
-
-            // Call the method to update available grave numbers
-            $this->load_data();
-
-            // Reset the form input values
-            $this->selected_grave = null;
-            $this->name = null;
-            $this->surname = null;
-            $this->date_of_birth = null;
-            $this->date_of_death = null;
-
-            $this->dispatchBrowserEvent('swal', [
-                'title' => 'Record Added',
-                'icon' => 'success',
-                'iconColor' => 'green',
-            ]);
-        }
+        
     }
 }
