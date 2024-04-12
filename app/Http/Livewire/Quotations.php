f41c8c0bc1b9e43dcb50fdab7e192b05daa71dfd
ralphd <ralphd497@gmail.com>
2024-04-12 10:24:58 +0200

    Revert "quotation updates"
    This reverts commit 5c188d4f529db21d76b95f9857eef71351a152e5.

--- app/Http/Livewire/Quotations.php
+++ app/Http/Livewire/Quotations.php
@@ -3,26 +3,11 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use App\Models\Orders; // Import the Order model
 
 class Quotations extends Component
 {
-    public $orders;
-
-    public function mount()
-    {
-        // Fetch orders from the database when the component is mounted
-        $orders = Orders::all();
-        
-        // Filter the orders to ensure only one entry for each UserId
-        $this->orders = $orders->unique('UserId');
-    }
-
     public function render()
     {
-        // Pass the orders data to the view
-        return view('livewire.quotations', [
-            'orders' => $this->orders,
-        ]);
+        return view('livewire.quotations');
     }
 }
