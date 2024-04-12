10d0d8dbf7ffe3110d3aa9dd9740ff316cc63f5c
ralphd <ralphd497@gmail.com>
2024-04-12 10:48:57 +0200

    Reapply "quotation updates"
    This reverts commit f41c8c0bc1b9e43dcb50fdab7e192b05daa71dfd.

--- resources/views/livewire/quotations.blade.php
+++ resources/views/livewire/quotations.blade.php
@@ -1,4 +1,5 @@
 <div>
+<<<<<<< Updated upstream
     <div class="card w-75 m-3 mx-auto">
         <div class="card-header"> Quotations </div>
         <div class="card-body">
@@ -9,5 +10,31 @@
 
     </div>
 
+=======
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Quotation Number</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Iterate over orders and display UserId -->
+                @foreach($orders as $order)
+                <tr>
+                    <td>{{ $order->UserId }}</td>
+                    <td>
+                        <!-- Button trigger modal -->
+                        <button wire:click="viewFullQuotation" type="button" class="btn btn-primary">View Full Quotation</button>
 
+                        <button wire:click="deleteQuotation" class="btn btn-danger">Delete Quotation</button>
+>>>>>>> Stashed changes
+
+                        <button wire:click="approveQuotation" class="btn btn-success">Approve Quotation</button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
