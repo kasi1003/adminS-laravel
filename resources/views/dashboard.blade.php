3395a421fc5097e83d985ae95d1d3e26b105ff4b
Hosea Noabeb Chris <122771226+Hozei@users.noreply.github.com>
2024-07-12 10:52:03 -0700

    Make Admin site responsive

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,39 +8,40 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-sm-12 col-md-6 mb-4">
                     <a href="{{ route('addGraveyards') }}" class="card-link">
-                        <div class="card m-3">
-                            <div class="card-body mx-auto" style="height: 10rem; display: flex; align-items: center;">
+                        <div class="card">
+                            <div class="card-body d-flex align-items-center justify-content-center" style="height: 10rem;">
                                 <h5 class="card-title">Manage Graveyards</h5>
                             </div>
                         </div>
                     </a>
                 </div>
 
-                <div class="col-md-6">
+                <div class="col-sm-12 col-md-6 mb-4">
                     <a href="{{ route('burialRecords') }}" class="card-link">
-
-                        <div class="card m-3">
-                            <div class="card-body mx-auto" style="height: 10rem; display:flex; align-items:center;">
+                        <div class="card">
+                            <div class="card-body d-flex align-items-center justify-content-center" style="height: 10rem;">
                                 <h5 class="card-title">Manage Burial Records</h5>
                             </div>
                         </div>
                     </a>
                 </div>
-                <div class="col-md-6">
+
+                <div class="col-sm-12 col-md-6 mb-4">
                     <a href="{{ route('serviceProviders') }}" class="card-link">
-                        <div class="card m-3">
-                            <div class="card-body mx-auto" style="height: 10rem ; display:flex; align-items:center;">
+                        <div class="card">
+                            <div class="card-body d-flex align-items-center justify-content-center" style="height: 10rem;">
                                 <h5 class="card-title">Service Providers</h5>
                             </div>
                         </div>
                     </a>
                 </div>
-                <div class="col-md-6">
+
+                <div class="col-sm-12 col-md-6 mb-4">
                     <a href="{{ route('quotations') }}" class="card-link">
-                        <div class="card m-3">
-                            <div class="card-body mx-auto" style="height: 10rem ; display:flex; align-items:center;">
+                        <div class="card">
+                            <div class="card-body d-flex align-items-center justify-content-center" style="height: 10rem;">
                                 <h5 class="card-title">Quotations</h5>
                             </div>
                         </div>
@@ -59,5 +60,4 @@
             transform: scale(1.05);
         }
     </style>
-
 </x-app-layout>
